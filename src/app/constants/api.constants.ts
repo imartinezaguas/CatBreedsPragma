d4e8d09e9475903a533c114bea5644aef39f936c
Ivan Martinez <ingeniero.ivan92@gmail.com>
2025-06-25 16:45:12 -0500

    feat:Add Constant IMG_UNKNOWN

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,1 +1,2 @@
 export const API_LIMIT = 10;
+export const IMG_UNKNOWN = 'assets/image/unknown.png'
--- src/app/pipes/image-fallback.pipe.ts
+++ src/app/pipes/image-fallback.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IMG_UNKNOWN } from '../constants/api.constants';
 
 @Pipe({
   name: 'imageFallback'
 })
 export class ImageFallbackPipe implements PipeTransform {
 
-   transform(image: { url?: string } | null | undefined, fallback: string = 'assets/image/unknown.png'): string {
+   transform(image: { url?: string } | null | undefined, fallback: string = IMG_UNKNOWN): string {
     return image?.url?.trim() ? image.url : fallback;
   }
 
